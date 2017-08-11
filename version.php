63eb04629d88b6934095c95a53944cd6b7ba483c
Kathrin Osswald <kathrin.osswald@uni-ulm.de>
2017-08-11 11:58:41 +0200

    Release v3.2-r5

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_navbarplus';
 $plugin->version = 2017081100;
-$plugin->release = 'v3.2-r4';
+$plugin->release = 'v3.2-r5';
 $plugin->requires = 2016120500;
 $plugin->maturity = MATURITY_STABLE;
