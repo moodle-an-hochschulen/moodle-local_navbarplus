2a7f42ae113620e7f6308f3da7b51d7f30749f95
Kathrin Osswald <kathrin.osswald@uni-ulm.de>
2019-06-19 07:41:34 +0200

    Added Behat tests.

---
+++ tests/behat/behat_local_navbarplus.php
@@ -0,0 +1,209 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Steps definitions for local_navbarplus
+ *
+ * This script is only called from Behat as part of it's integration
+ * in Moodle.
+ *
+ * @package   local_navbarplus
+ * @category  test
+ * @copyright 2019 Kathrin Osswald <kathrin.osswald@uni-ulm.de>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// NOTE: no MOODLE_INTERNAL test here, this file may be required by behat before including /config.php.
+
+
+/**
+ * Steps definitions for local_navbarplus
+ *
+ * This script is only called from Behat as part of it's integration
+ * in Moodle.
+ *
+ * @package   local_navbarplus
+ * @category  test
+ * @copyright 2019 Kathrin Osswald <kathrin.osswald@uni-ulm.de>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class behat_local_navbarplus extends behat_base {
+    /**
+     * Checks, that the specified element with this title, link and iconclass attribute is existent on the page.
+     *
+     * @codingStandardsIgnoreLine
+     * @Then /^I should see the icon with the title "(?P<title_string>(?:[^"]|\\")*)" and the iconclass "(?P<icon_string>(?:[^"]|\\")*)" and the link "(?P<link_string>(?:[^"]|\\")*)" in the navbar$/
+     *
+     * @param string $title
+     * @param string $icon
+     * @param string $link
+     */
+    public function assert_element_in_navbar_contains_title_iconclass_link($title, $icon, $link) {
+
+        // We are searching for our icons in the navbar.
+        $elementxpath = '//ul[contains(@class, "navbar-nav")]';
+        $elementxpath .= '/li[contains(@class, "nav-item")]';
+        $elementxpath .= '/descendant-or-self::a[@title="'. $title . '"][contains(@href, "' . $link . '")]';
+        $elementxpath .= '/descendant::i[contains(@class, "' . $icon . '")]';
+
+        // Check if the element exists.
+        $this->execute("behat_general::should_exist",
+            array($elementxpath, "xpath_element"));
+    }
+
+    /**
+     * Checks, that the specified element with this title, link and iconclass attribute is not existent on the page.
+     *
+     * @codingStandardsIgnoreLine
+     * @Then /^I should not see the icon with the title "(?P<title_string>(?:[^"]|\\")*)" and the iconclass "(?P<icon_string>(?:[^"]|\\")*)" and the link "(?P<link_string>(?:[^"]|\\")*)" in the navbar$/
+     *
+     * @param string $title
+     * @param string $icon
+     * @param string $link
+     */
+    public function assert_element_in_navbar_not_contains_title_iconclass_link($title, $icon, $link) {
+
+        // We are searching for our icons in the navbar.
+        $elementxpath = '//ul[contains(@class, "navbar-nav")]';
+        $elementxpath .= '/li[contains(@class, "nav-item")]';
+        $elementxpath .= '/descendant-or-self::a[@title="'. $title . '"][contains(@href, "' . $link . '")]';
+        $elementxpath .= '/descendant::i[contains(@class, "' . $icon . '")]';
+
+        // Check if the element does not exist.
+        $this->execute("behat_general::should_not_exist",
+                array($elementxpath, "xpath_element"));
+    }
+
+    /**
+     *  Checks, that the specified element with this title attribute is existent on the page.
+     *
+     * @Then /^I should see the icon with the title "(?P<title_string>(?:[^"]|\\")*)" in the navbar$/
+     *
+     * @param string $title
+     */
+    public function assert_element_in_navbar_contains_title($title) {
+
+        // We are searching for our icons in the navbar.
+        $elementxpath = '//ul[contains(@class, "navbar-nav")]';
+        $elementxpath .= '/li[contains(@class, "nav-item")]';
+        $elementxpath .= '/descendant-or-self::a[@title="'. $title . '"]';
+
+        // Check if the element exists.
+        $this->execute("behat_general::should_exist",
+            array($elementxpath, "xpath_element"));
+    }
+
+    /**
+     * Checks, that the specified element with this title attribute is not existent on the page.
+     *
+     * @Then /^I should not see the icon with the title "(?P<title_string>(?:[^"]|\\")*)" in the navbar$/
+     *
+     * @param string $title
+     */
+    public function assert_element_in_navbar_not_contains_title($title) {
+
+        // We are searching for our icons in the navbar.
+        $elementxpath = '//ul[contains(@class, "navbar-nav")]';
+        $elementxpath .= '/li[contains(@class, "nav-item")]';
+        $elementxpath .= '/descendant-or-self::a[@title="'. $title . '"]';
+
+        // Check if the element does not exist.
+        $this->execute("behat_general::should_not_exist",
+            array($elementxpath, "xpath_element"));
+    }
+
+    /**
+     * Checks, that the specified element is existent and has new window attribute.
+     *
+     * @Then /^I should see the icon with the title "(?P<title_string>(?:[^"]|\\")*)" with the new window option in the navbar$/
+     *
+     * @param string $title
+     */
+    public function assert_element_in_navbar_has_new_window_attribute($title) {
+
+        // We are searching for our icons in the navbar.
+        $elementxpath = '//ul[contains(@class, "navbar-nav")]';
+        $elementxpath .= '/li[contains(@class, "nav-item")]';
+        $elementxpath .= '/descendant-or-self::a[@title="'. $title . '"][contains(@target, "_blank")]';
+
+        // Check if the element exists.
+        $this->execute("behat_general::should_exist",
+            array($elementxpath, "xpath_element"));
+    }
+
+    /**
+     * Checks, that the specified element is existent and has new window attribute.
+     *
+     * @Then /^I should see the icon with the title "(?P<title_string>(?:[^"]|\\")*)" without the new window option in the navbar$/
+     *
+     * @param string $title
+     */
+    public function assert_element_in_navbar_not_has_new_window_attribute($title) {
+
+        // We are searching for our icons in the navbar.
+        $elementxpath = '//ul[contains(@class, "navbar-nav")]';
+        $elementxpath .= '/li[contains(@class, "nav-item")]';
+        $elementxpath .= '/descendant-or-self::a[@title="'. $title . '"][contains(@target, "_blank")]';
+
+        // Check if the element exists.
+        $this->execute("behat_general::should_not_exist",
+            array($elementxpath, "xpath_element"));
+    }
+
+    /**
+     * Checks, that the specified element is existent and has additional classes attribute.
+     *
+     * @codingStandardsIgnoreLine
+     * @Then /^I should see the icon with the title "(?P<title_string>(?:[^"]|\\")*)" and the optional class "(?P<class_string>(?:[^"]|\\")*)" in the navbar$/
+     *
+     * @param string $title
+     * @param string $class
+     */
+    public function assert_element_in_navbar_has_additional_class($title, $class) {
+
+        // We are searching for our icons in the navbar.
+        $elementxpath = '//ul[contains(@class, "navbar-nav")]';
+        $elementxpath .= '/li[contains(@class, "nav-item")]';
+        $elementxpath .= '/descendant::div[contains(@class, "' . $class . '")]';
+        $elementxpath .= '/descendant-or-self::a[@title="'. $title . '"]';
+
+        // Check if the element exists.
+        $this->execute("behat_general::should_exist",
+            array($elementxpath, "xpath_element"));
+    }
+
+    /**
+     * Checks, that the specified element is existent and has additional id attribute.
+     *
+     * @codingStandardsIgnoreLine
+     * @Then /^I should see the icon with the title "(?P<title_string>(?:[^"]|\\")*)" and the optional id "(?P<id_string>(?:[^"]|\\")*)" in the navbar$/
+     *
+     * @param string $title
+     * @param string $id
+     */
+    public function assert_element_in_navbar_has_additional_id($title, $id) {
+
+        // We are searching for our icons in the navbar.
+        $elementxpath = '//ul[contains(@class, "navbar-nav")]';
+        $elementxpath .= '/li[contains(@class, "nav-item")]';
+        $elementxpath .= '/descendant::div[contains(@id, "' . $id . '")]';
+        $elementxpath .= '/descendant-or-self::a[@title="'. $title . '"]';
+
+        // Check if the element exists.
+        $this->execute("behat_general::should_exist",
+            array($elementxpath, "xpath_element"));
+    }
+}
---
+++ tests/behat/configuring_navbarplus.feature
@@ -0,0 +1,83 @@
+@local @local_navbarplus
+Feature: Configuring the navbarplus plugin
+  In order to have custom items in the additional navbar
+  As admin
+  I need to be able to configure the navbarplus plugin
+
+  Background:
+    And the following "users" exist:
+      | username | lang |
+      | student1 | de   |
+
+  Scenario: Configuring item with mandatory attributes
+    When I log in as "admin"
+    When I navigate to "Appearance > Navbar Plus" in site administration
+    And I set the field "id_s_local_navbarplus_inserticonswithlinks" to "fa-sign-out|/login/logout.php|Logout"
+    And I press "Save"
+    And I should see the icon with the title "Logout" and the iconclass "fa-sign-out" and the link "/login/logout.php" in the navbar
+
+  Scenario: Configuring item with less than mandatory attributes
+    When I log in as "admin"
+    When I navigate to "Appearance > Navbar Plus" in site administration
+    And I set the field "id_s_local_navbarplus_inserticonswithlinks" to "Falsetest"
+    And I press "Save"
+    And I should not see the icon with the title "Falsetest" in the navbar
+
+  Scenario: Configuring item with additional language attribute
+    When I log in as "admin"
+    And I navigate to "Language > Language packs" in site administration
+    And I set the field "Available language packs" to "de"
+    And I press "Install selected language pack(s)"
+    When I navigate to "Appearance > Navbar Plus" in site administration
+    And I set the field "id_s_local_navbarplus_inserticonswithlinks" to "fa-language|/?redirect=0|Languagetest|de"
+    And I press "Save"
+    Then I should not see the icon with the title "Languagetest" and the iconclass "fa-language" and the link "/?redirect=0" in the navbar
+    And I log out
+    When I log in as "student1"
+    Then I should see the icon with the title "Languagetest" and the iconclass "fa-language" and the link "/?redirect=0" in the navbar
+
+  Scenario: Configuring item with the new window attribute
+    When I log in as "admin"
+    And I navigate to "Appearance > Navbar Plus" in site administration
+    And I set the field "id_s_local_navbarplus_inserticonswithlinks" to "fa-window-maximize|/?redirect=0|Newwindowtest||true"
+    And I press "Save"
+    Then I should see the icon with the title "Newwindowtest" with the new window option in the navbar
+
+  Scenario: Configuring item with mandatory attributes and check that they don't have the new window attribute
+    When I log in as "admin"
+    When I navigate to "Appearance > Navbar Plus" in site administration
+    And I set the field "id_s_local_navbarplus_inserticonswithlinks" to "fa-sign-in|/login/logout.php|Login"
+    And I press "Save"
+    And I should see the icon with the title "Login" without the new window option in the navbar
+
+  Scenario: Configuring item with additional class attribute
+    When I log in as "admin"
+    And I navigate to "Appearance > Navbar Plus" in site administration
+    And I set the field "id_s_local_navbarplus_inserticonswithlinks" to "fa-glass|/?redirect=0|Classtest|||optional-class"
+    And I press "Save"
+    Then I should see the icon with the title "Classtest" and the optional class "optional-class" in the navbar
+
+  Scenario: Configuring item with additional id attribute
+    When I log in as "admin"
+    And I navigate to "Appearance > Navbar Plus" in site administration
+    And I set the field "id_s_local_navbarplus_inserticonswithlinks" to "fa-id-card|/?redirect=0|Idtest||||optional-id"
+    And I press "Save"
+    Then I should see the icon with the title "Idtest" and the optional id "optional-id" in the navbar
+
+  Scenario: Enabling the link to show the reset users tour link in the navbar if a user tour is created for that page
+    When I log in as "admin"
+    And I navigate to "Appearance > Navbar Plus" in site administration
+    And I set the field "Reset user tour link" to "Yes"
+    And I press "Save"
+    And I add a new user tour with:
+      | Name                | First tour |
+      | Description         | My first tour |
+      | Apply to URL match  | /my/% |
+      | Tour is enabled     | 1 |
+    And I add steps to the "First tour" tour:
+      | targettype                  | Title             | Content |
+      | Display in middle of page   | Welcome           | Welcome to your personal learning space. We'd like to give you a quick tour to show you some of the areas you may find helpful |
+    When I am on homepage
+    Then I should see the icon with the title "Reset user tour on this page (Could take a short time)" in the navbar
+    When I am on site homepage
+    Then I should not see the icon with the title "Reset user tour on this page (Could take a short time)" in the navbar
