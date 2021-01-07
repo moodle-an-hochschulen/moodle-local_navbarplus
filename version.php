d9689522a138946a888fe951d837d09f5650296b
Alexander Bias <alexander.bias@uni-ulm.de>
2021-01-07 21:18:23 +0100

    Improvement: Declare which major stable version of Moodle this plugin supports

--- version.php
+++ version.php
@@ -28,4 +28,5 @@ $plugin->component = 'local_navbarplus';
 $plugin->version = 2020071700;
 $plugin->release = 'v3.9-r1';
 $plugin->requires = 2020061500;
+$plugin->supported = [39, 39];
 $plugin->maturity = MATURITY_STABLE;
