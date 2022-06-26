1d785337ddf69008e1b13d95f63c79bd40c60758
Alexander Bias <bias@alexanderbias.de>
2022-06-26 15:37:13 +0200

    Make codechecker happy again

--- lib.php
+++ lib.php
@@ -22,8 +22,6 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * Allow plugins to provide some content to be rendered in the navbar.
  * The plugin must define a PLUGIN_render_navbar_output function that returns
