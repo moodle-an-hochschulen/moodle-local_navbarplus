9a7105d500435c1318728c1d88f3aecdcd906000
Alexander Bias <alexander.bias@lernlink.de>
2022-06-26 18:16:09 +0200

    Release v3.11-r2

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_navbarplus';
-$plugin->version = 2021120400;
-$plugin->release = 'v3.11-r1';
+$plugin->version = 2021120401;
+$plugin->release = 'v3.11-r2';
 $plugin->requires = 2021051700;
 $plugin->supported = [311, 311];
 $plugin->maturity = MATURITY_STABLE;
