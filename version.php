84fd42c7606218da0a0fde2b3c6ed047c2a87578
Alexander Bias <bias@alexanderbias.de>
2022-07-18 22:15:56 +0200

    Release v3.11-r3

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_navbarplus';
-$plugin->version = 2021120401;
-$plugin->release = 'v3.11-r2';
+$plugin->version = 2021120402;
+$plugin->release = 'v3.11-r3';
 $plugin->requires = 2021051700;
 $plugin->supported = [311, 311];
 $plugin->maturity = MATURITY_STABLE;
